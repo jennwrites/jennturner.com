4c4aa1649c1dfb825c703860e3b026147819e988
Jenn Turner <renrutnnej@gmail.com>
2026-09-14 19:26:35 -0500

    Add dynamic favicon selection logic

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// 1. Specify the total number of favicons in your folder
+const totalFavicons = 37; 
+
+// 2. Generate a random integer between 1 and your total number
+const randomNumber = Math.floor(Math.random() * totalFavicons) + 1;
+
+// 3. Target the link tag and swap the path
+const favicon = document.getElementById('dynamic-favicon');
+favicon.href = `favicons/favicon${randomNumber}.png`;
 
